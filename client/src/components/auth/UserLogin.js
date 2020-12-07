9b4d8108e072595d42d424a02be1606b6cab5e0e
unknown <avinashupadhyay56@gmail.com>
2020-12-07 14:44:51 +0530

    Front-end completed

--- client/src/components/auth/UserLogin.js
+++ client/src/components/auth/UserLogin.js
@@ -1,25 +1,24 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import Login from "./Login";
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 // import { loadUser } from "../actions/auth";
 
-
-const UserLogin = ({isAuthenticated}) => {
- if(isAuthenticated){
-       return <Redirect to='/user-dashboard'/>
-}
+const UserLogin = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to="/user-dashboard" />;
+  }
 
   return (
     <div>
       <Login />
       <h5>Email: test1@gmail.com </h5>
-        <h5>Password: 123456</h5> <p>Optional(You can register new user as well)</p>
-        
-      <h3 className='heading'>
-
-        Sign as the the Founder. You can register  your startup 
+      <h5>Password: 123456</h5>{" "}
+      <p>Optional(You can register new user as well)</p>
+      
+      <h3 className="heading">
+        Sign as the the Founder. You can register your startup
       </h3>
       {/* <img src={candidateImage} className='image'></img> */}
     </div>
@@ -33,8 +32,8 @@ const UserLogin = ({isAuthenticated}) => {
 //     isAuthenticated : state.auth.isAuthenticated
 // })
 
-const mapSTateToProps = state =>({
- isAuthenticated: state.auth.isAuthenticated
-})
+const mapSTateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
 
-export default connect(mapSTateToProps)(UserLogin)
+export default connect(mapSTateToProps)(UserLogin);
